dd1a1e8106afb788096a9ceddce55d17ecbfec8a
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-03-02 12:45:08 +0500

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+ let name = "This is "
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+ <nav className="navbar navbar-expand-lg navbar-light bg-dark text-white">
+  <div className="container-fluid px-5">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse " id="navbarTogglerDemo01">
+      <a className="navbar-brand text-white fs-1 " href="/">Brand Logo</a>
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="/">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active text-white" href="/" tabindex="-1" aria-disabled="true">Services</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active text-white" href="/" tabindex="-1" aria-disabled="true">Contact</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active text-white" href="/" tabindex="-1" aria-disabled="true">Blog</a>
+        </li>
+      </ul>
+    
     </div>
+  </div>
+</nav>
+    </>
   );
 }
 
